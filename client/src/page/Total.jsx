86e07129d459cbc05ae6cfd641f0ae504fc73a59
Narutchai <koonfluk2015@gmail.com>
2023-08-24 16:01:40 +0700

    wait a deploy

--- client/src/page/Total.jsx
+++ client/src/page/Total.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useState } from "react";
 import logo from "../assets/log_acs.png";
 const Total = () => {
@@ -10,56 +9,6 @@ const Total = () => {
         const res = await fetch("http://localhost:3000/cocdata");
         const data = await res.json();
         setData(data);
-=======
-
-import { useEffect, useState } from "react"
-
-
-const Total = () => {
-
-    const [apiData, setApiData] = useState([]);
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch('https://acs-coc-api.wachawich.repl.co/api/totalscore');
-            const data = await response.json();
-            setApiData(data);
-          } catch (error) {
-            console.error('Error fetching API data:', error);
-          }
-        };
-    
-        fetchData();
-      }, []);
-
-    const combineScoresById = () => {
-        const combinedScores = {};
-
-        apiData.forEach(user => {
-            user.coc_data.forEach(coc => {
-              coc.match_data.forEach(match => {
-                match.player.forEach(player => {
-                  const playerId = player.id;
-                  if (!combinedScores[playerId]) {
-                    combinedScores[playerId] = {
-                      nickname: player.codingamerNickname,
-                      totalScore: 0,
-                    };
-                  }
-                  combinedScores[playerId].totalScore += player.score;
-                });
-              });
-            });
-          });
-
-        const combinedScoresArray = Object.values(combinedScores);
-
-        // เรียงลำดับตามคะแนนที่มากที่สุด
-        combinedScoresArray.sort((a, b) => b.totalScore - a.totalScore);
-        return combinedScoresArray;
->>>>>>> main
     };
     // console.log(data);
 
@@ -99,7 +48,6 @@ const Total = () => {
 
     return (
         <>
-<<<<<<< HEAD
             <div className="container mx-auto px-36">
                 <div className="flex justify-center">
                     <img src={logo} alt="" width={500} />
@@ -112,25 +60,6 @@ const Total = () => {
                                     <th>Rank</th>
                                     <th>Name</th>
                                     <th>Score</th>
-=======
-            <h1 className="text-center text-6xl font-bold">Overall</h1>
-            <div className="mt-10 container mx-auto px-4 h-screen">
-                <div className="flex justify-center pt-6 h-80">
-                    <table className="text-center w-full">
-                        <thead>
-                            <tr className=" text-3xl">
-                                <th>Rank</th>
-                                <th>Player</th>
-                                <th>Score</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {combinedScoresArray.map((player, index) => (
-                                <tr key={player.nickname} className="text-2xl">
-                                    <td>{index + 1}</td>
-                                    <td>{player.nickname}</td>
-                                    <td>{player.totalScore}</td>
->>>>>>> main
                                 </tr>
                             </thead>
                             <tbody className="text-[23px] text-center text-white [&>*:nth-child(2n)]:bg-[#291456] [&>*:nth-child(2n+1)]:bg-[#35246A]">
