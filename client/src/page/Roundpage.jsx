46ba2fee939a493258f0249b138fcd626ae69448
Narutchai <39022151+Narutchai01@users.noreply.github.com>
2023-09-22 23:03:28 +0700

    update 3 table and backend

--- client/src/page/Roundpage.jsx
+++ client/src/page/Roundpage.jsx
@@ -7,8 +7,10 @@ const Roundpage = () => {
     const { dateId } = useParams();
     const [match1, setMatch1] = useState({});
     const [match2, setMatch2] = useState({});
+    const [match3, setMatch3] = useState({});
     let round1 = {}
     let round2 = {}
+    let round3 = {}
 
 
     const getMatch1 = async (dateId) => {
@@ -23,15 +25,24 @@ const Roundpage = () => {
         setMatch2(data);
     }
 
+    const getMatch3 = async (dateId) => {
+        const res = await fetch(`https://acs-api-coc.onrender.com/cocdata/${dateId}/2/data_match`);
+        const data = await res.json();
+        setMatch3(data);
+    };
+
     round1 = match1.players
     round2 = match2.players
+    round3 = match3.players
 
     console.log(round1);
     console.log(round2);
+    console.log(round3);
 
     useEffect(() => {
         getMatch1(dateId);
         getMatch2(dateId);
+        getMatch3(dateId);
     }, [dateId]);
 
     const Showmatch1 = round1?.map((player) => {
@@ -54,13 +65,26 @@ const Roundpage = () => {
             </tr>
         )
     })
+    const Showmatch3 = round3?.map((player) => {
+        const score = 100 - (player.rank - 1) * 100 / match3.players.length
+        return (
+            <tr key={player.codingamerId}>
+                <td>{player.rank}</td>
+                <td>{player.codingamerNickname}</td>
+                <td>{score}</td>
+            </tr>
+        )
+    })
 
     return (
         <>
             <div className='container mx-auto px-36'>
                 <div className='bg-black px-5 py-5 rounded-3xl bg-opacity-[20%]'>
-                    <NavScore />
-                    <div className='grid grid-cols-2 gap-[30px] justify-center h-[400px] mt-10'>
+                    <div className='mb-10'>
+                        <NavScore />
+                    </div>
+                    <div className='flex flex-wrap gap-10'>
+                        <h1 className='text-white font-bold text-3xl'>Round 1</h1>
                         <div className='bg-[#291456] text-white w-full h-auto py-3 rounded-2xl'>
                             <table className='w-full h-full'>
                                 <thead>
@@ -75,6 +99,8 @@ const Roundpage = () => {
                                 </tbody>
                             </table>
                         </div>
+
+                        <h1 className='text-white font-bold text-3xl'>Round 1</h1>
                         <div className='bg-[#291456] text-white w-full h-auto py-3 rounded-2xl'>
                             <table className='w-full h-full'>
                                 <thead>
@@ -86,6 +112,24 @@ const Roundpage = () => {
                                 </thead>
                                 <tbody className='text-center text-[20px] [&>*:nth-child(2n)]:bg-[#291456] [&>*:nth-child(2n+1)]:bg-[#35246A] '>
                                     {Showmatch2}
+                                </tbody>
+                            </table>
+                        </div>
+
+                        
+                        
+                        <h1 className='text-white font-bold text-3xl'>Round 1</h1>
+                        <div className='bg-[#291456] text-white w-full h-auto py-3 rounded-2xl'>
+                            <table className='w-full h-full'>
+                                <thead>
+                                    <tr>
+                                        <th>Rank</th>
+                                        <th>Name</th>
+                                        <th>Score</th>
+                                    </tr>
+                                </thead>
+                                <tbody className='text-center text-[20px] [&>*:nth-child(2n)]:bg-[#291456] [&>*:nth-child(2n+1)]:bg-[#35246A] '>
+                                    {Showmatch3}
                                 </tbody>
                             </table>
                         </div>
--- server/server.js
+++ server/server.js
@@ -12,175 +12,19 @@ const products = [
             {
                 match_id: "1",
                 match_data: [
-                   {
-    "nbPlayersMin": 2,
-    "nbPlayersMax": 100,
-    "publicHandle": "324238669616f08b3300783bad63664dad17931",
-    "clashDurationTypeId": "SHORT",
-    "mode": "REVERSE",
-    "creationTime": "Aug 25, 2023, 2:33:31 PM",
-    "startTime": "Aug 25, 2023, 2:37:44 PM",
-    "endTime": "Aug 25, 2023, 2:52:44 PM",
-    "startTimestamp": 1692974264715,
-    "msBeforeStart": -220087,
-    "msBeforeEnd": 679912,
-    "finished": false,
-    "started": true,
-    "publicClash": false,
-    "players": [
-        {
-            "codingamerId": 4388865,
-            "codingamerNickname": "VarinPond",
-            "codingamerHandle": "e07a034861180d16d6e1c9869e5f9d0b5688834",
-            "codingamerAvatarId": 68202409504423,
-            "score": 100,
-            "duration": 100090,
-            "status": "OWNER",
-            "testSessionStatus": "COMPLETED",
-            "languageId": "Python3",
-            "rank": 1,
-            "position": 1,
-            "solutionShared": true,
-            "testSessionHandle": "6191531510bb4b70c162b3908afad515cafac3a7",
-            "submissionId": 31603232
-        },
-        {
-            "codingamerId": 5563840,
-            "codingamerNickname": "H4aRu",
-            "codingamerHandle": "5efface12a3229efe410c4bcca674daf0483655",
-            "duration": 0,
-            "status": "STANDARD",
-            "testSessionStatus": "READY",
-            "rank": 2,
-            "position": 2,
-            "testSessionHandle": "61915316e312df42cd77d4a78feb73d3dbca5e15"
-        }
-    ],
-    "programmingLanguages": [
-        "Bash",
-        "C",
-        "C#",
-        "C++",
-        "Clojure",
-        "D",
-        "Dart",
-        "F#",
-        "Go",
-        "Groovy",
-        "Haskell",
-        "Java",
-        "Javascript",
-        "Kotlin",
-        "Lua",
-        "ObjectiveC",
-        "OCaml",
-        "Pascal",
-        "Perl",
-        "PHP",
-        "Python3",
-        "Ruby",
-        "Rust",
-        "Scala",
-        "Swift",
-        "TypeScript",
-        "VB.NET"
-    ],
-    "modes": [
-        "FASTEST",
-        "REVERSE",
-        "SHORTEST"
-    ],
-    "type": "PRIVATE"
-}
+
                 ]
             },
             {
                 match_id: "2",
                 match_data: [
-                    {
-    "nbPlayersMin": 2,
-    "nbPlayersMax": 100,
-    "publicHandle": "324238669616f08b3300783bad63664dad17931",
-    "clashDurationTypeId": "SHORT",
-    "mode": "REVERSE",
-    "creationTime": "Aug 25, 2023, 2:33:31 PM",
-    "startTime": "Aug 25, 2023, 2:37:44 PM",
-    "endTime": "Aug 25, 2023, 2:52:44 PM",
-    "startTimestamp": 1692974264715,
-    "msBeforeStart": -12308351,
-    "msBeforeEnd": -11408351,
-    "finished": true,
-    "started": true,
-    "publicClash": false,
-    "players": [
-        {
-            "codingamerId": 4388865,
-            "codingamerNickname": "VarinPond",
-            "codingamerHandle": "e07a034861180d16d6e1c9869e5f9d0b5688834",
-            "codingamerAvatarId": 68202409504423,
-            "score": 100,
-            "duration": 100090,
-            "status": "OWNER",
-            "testSessionStatus": "COMPLETED",
-            "languageId": "Python3",
-            "rank": 1,
-            "position": 1,
-            "solutionShared": true,
-            "testSessionHandle": "6191531510bb4b70c162b3908afad515cafac3a7",
-            "submissionId": 31603232
-        },
-        {
-            "codingamerId": 5563840,
-            "codingamerNickname": "H4aRu",
-            "codingamerHandle": "5efface12a3229efe410c4bcca674daf0483655",
-            "score": 0,
-            "duration": 900000,
-            "status": "STANDARD",
-            "testSessionStatus": "COMPLETED",
-            "languageId": "undefined",
-            "rank": 2,
-            "position": 2,
-            "solutionShared": false,
-            "testSessionHandle": "61915316e312df42cd77d4a78feb73d3dbca5e15",
-            "submissionId": 31603389
-        }
-    ],
-    "programmingLanguages": [
-        "Bash",
-        "C",
-        "C#",
-        "C++",
-        "Clojure",
-        "D",
-        "Dart",
-        "F#",
-        "Go",
-        "Groovy",
-        "Haskell",
-        "Java",
-        "Javascript",
-        "Kotlin",
-        "Lua",
-        "ObjectiveC",
-        "OCaml",
-        "Pascal",
-        "Perl",
-        "PHP",
-        "Python3",
-        "Ruby",
-        "Rust",
-        "Scala",
-        "Swift",
-        "TypeScript",
-        "VB.NET"
-    ],
-    "modes": [
-        "FASTEST",
-        "REVERSE",
-        "SHORTEST"
-    ],
-    "type": "PRIVATE"
-}
+
+                ]
+            },
+            {
+                match_id: "3",
+                match_data: [
+
                 ]
             }
         ]
@@ -191,95 +35,14 @@ const products = [
             {
                 match_id: "1",
                 match_data: [
-                    {
-    "nbPlayersMin": 2,
-    "nbPlayersMax": 100,
-    "publicHandle": "324238669616f08b3300783bad63664dad17931",
-    "clashDurationTypeId": "SHORT",
-    "mode": "REVERSE",
-    "creationTime": "Aug 25, 2023, 2:33:31 PM",
-    "startTime": "Aug 25, 2023, 2:37:44 PM",
-    "endTime": "Aug 25, 2023, 2:52:44 PM",
-    "startTimestamp": 1692974264715,
-    "msBeforeStart": -12308351,
-    "msBeforeEnd": -11408351,
-    "finished": true,
-    "started": true,
-    "publicClash": false,
-    "players": [
-        {
-            "codingamerId": 4388865,
-            "codingamerNickname": "VarinPond",
-            "codingamerHandle": "e07a034861180d16d6e1c9869e5f9d0b5688834",
-            "codingamerAvatarId": 68202409504423,
-            "score": 100,
-            "duration": 100090,
-            "status": "OWNER",
-            "testSessionStatus": "COMPLETED",
-            "languageId": "Python3",
-            "rank": 1,
-            "position": 1,
-            "solutionShared": true,
-            "testSessionHandle": "6191531510bb4b70c162b3908afad515cafac3a7",
-            "submissionId": 31603232
-        },
-        {
-            "codingamerId": 5563840,
-            "codingamerNickname": "H4aRu",
-            "codingamerHandle": "5efface12a3229efe410c4bcca674daf0483655",
-            "score": 0,
-            "duration": 900000,
-            "status": "STANDARD",
-            "testSessionStatus": "COMPLETED",
-            "languageId": "undefined",
-            "rank": 2,
-            "position": 2,
-            "solutionShared": false,
-            "testSessionHandle": "61915316e312df42cd77d4a78feb73d3dbca5e15",
-            "submissionId": 31603389
-        }
-    ],
-    "programmingLanguages": [
-        "Bash",
-        "C",
-        "C#",
-        "C++",
-        "Clojure",
-        "D",
-        "Dart",
-        "F#",
-        "Go",
-        "Groovy",
-        "Haskell",
-        "Java",
-        "Javascript",
-        "Kotlin",
-        "Lua",
-        "ObjectiveC",
-        "OCaml",
-        "Pascal",
-        "Perl",
-        "PHP",
-        "Python3",
-        "Ruby",
-        "Rust",
-        "Scala",
-        "Swift",
-        "TypeScript",
-        "VB.NET"
-    ],
-    "modes": [
-        "FASTEST",
-        "REVERSE",
-        "SHORTEST"
-    ],
-    "type": "PRIVATE"
-}
+
                 ]
             },
             {
                 match_id: "2",
-                match_data: []
+                match_data: [
+
+                ]
             }
         ]
     },
@@ -288,11 +51,15 @@ const products = [
         coc_data: [
             {
                 match_id: "1",
-                match_data: []
+                match_data: [
+
+                ]
             },
             {
                 match_id: "2",
-                match_data: []
+                match_data: [
+
+                ]
             }
         ]
     },
@@ -301,11 +68,15 @@ const products = [
         coc_data: [
             {
                 match_id: "1",
-                match_data: []
+                match_data: [
+
+                ]
             },
             {
                 match_id: "2",
-                match_data: []
+                match_data: [
+                    
+                ]
             }
         ]
     }
