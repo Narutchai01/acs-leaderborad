0801f9746414a862779d455c439e48269dd13424
Narutchai <39022151+Narutchai01@users.noreply.github.com>
2023-09-23 21:07:14 +0700

    fixbug socre

--- client/src/page/Roundpage.jsx
+++ client/src/page/Roundpage.jsx
@@ -51,7 +51,7 @@ const Roundpage = () => {
             <tr key={player.codingamerId}>
                 <td>{player.rank}</td>
                 <td>{player.codingamerNickname}</td>
-                <td>{score}</td>
+                <td>{parseInt(score)}</td>
             </tr>
         )
     })
