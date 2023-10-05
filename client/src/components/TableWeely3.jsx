fe72755d3dec1a5693a7d0afb4e85835a0ff62a8
Narutchai <39022151+Narutchai01@users.noreply.github.com>
2023-10-05 20:48:00 +0700

    update dynamic

---
+++ client/src/components/TableWeely3.jsx
@@ -0,0 +1,16 @@
+const TableWeely3 = () => {
+    return (
+        <>
+            <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Round 1</th>
+                <th>Round 2</th>
+                <th>Round 3</th>
+                <th>Total</th>
+            </tr>
+        </>
+    )
+}
+
+export default TableWeely3
